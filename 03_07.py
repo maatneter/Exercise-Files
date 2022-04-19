b76d184699a551888b0e4850df8064418bbcd362
maatneter <95445696+maatneter@users.noreply.github.com>
2022-04-19 14:18:08 -0400

    Add files via upload

---
+++ 03_07.py
@@ -0,0 +1,28 @@
+
+print("Challenge 1:")
+
+# A message for the user
+message = "This is going to be tricky ;)"
+Message = "Very tricky!"
+print(message) # show the message on the screen
+
+# Perform mathematical operations
+result = 2**3
+print("2**3 =", result)
+result = 5 - 3
+#print("5 - 3 =", result)
+
+print("Challenge complete!")
+
+print("Challenge 2:")
+
+message1 = "Hope you're having fun!"
+print("The message:", message1)
+
+answer = 5+2**3
+print("the answer is:", answer)
+
+result = 5 - 3
+#print("5 - 3 =", result)
+
+print("Done!")
---
+++ 04_03_begin.py
@@ -0,0 +1,9 @@
+plant = "Irises"
+
+if plant == "Cacti":
+    print(plant, "don't need a lot of water")
+else:
+    print(plant, "love water")
+
+print("Thanks!")
+ 
